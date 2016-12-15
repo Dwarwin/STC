38415807afe775d25ad05689d3fefb394541f921
Dmitriy <dwarwin@gmail.com>
2016-12-15 21:38:58 +0200

    Lots+fix

--- app/js/lots.js
+++ app/js/lots.js
@@ -30,12 +30,37 @@ $.ajax({
 
         function render(data) {
             let s = '',
-                lots='';
-            data.forEach(s => lots += `<section class="col-sm-3 lotInfo">
-				<a class="toLotDescr" id="${'lot' + s.id}"><img class="img-responsive lotImg" alt="lotImage" src= "img/lots/${s.id + ".jpg"}">
-				<h3>${s.name}</h3>
-				</a>
-				</section>`);
+                lots = '';
+            data.forEach(s => lots +=
+                `<section class="col-md-3 col-sm-4 col-xs-6 lotInfo">
+				    <a class="toLotDescr" data-toggle="modal" data-target="${'#modalLot' + s.id}" id="${'lot' + s.id}">
+				        <img class="img-responsive lotImg" alt="lotImage" src= "img/lots/${s.id + ".jpg"}">
+				        <h3>${s.name}</h3>
+				    </a>
+				</section>
+				
+				<section class="modal fade" id="${'modalLot' + s.id}" role="dialog">
+                    <div class="modal-dialog modal-lg">
+
+                    <section class="modal-content">
+                        <header class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h2 class="modal-title">${s.name}</h2>
+                        </header>
+                    <section class="modal-body">
+                    <div class="row lotInfo">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <img class="img-responsive lotImgDescr" alt="lotImageDescr" src= "img/lots/${s.id + ".jpg"}">
+                        </div>
+                        <div class="col-md-8 col-sm-6 col-xs-12">
+                            <p>${s.description}</p>
+                        </div>
+                     </div>
+                    </section>
+                    </section>
+                    </div>
+                </section>
+`);
 
             $('.lotList').html(lots);
 
